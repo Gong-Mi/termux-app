36997e6c705f62b9bc946a3380a487118434e099
Termux User <user@termux.local>
2026-03-28 19:35:52 +0800

    fix: Add missing Uri import and cleanup zombie git submodule

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
